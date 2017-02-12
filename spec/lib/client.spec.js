bb3ffa0169ee54ad70e2a38eb72ff06fa8e6d1cd
Michael Chacaton <chako256@gmail.com>
2017-02-12 01:45:16 +0100

    Added test for parser events

--- spec/lib/client.spec.js
+++ spec/lib/client.spec.js
@@ -1,4 +1,5 @@
 const Message = require('../../lib/message')
+const Parser = require('../../lib/parser')
 
 describe('Library test suite', () => {
     it('Message parsing', () => {
@@ -9,5 +10,19 @@ describe('Library test suite', () => {
         expect(message.params.length).toEqual(10)
         expect(message.trailing).toEqual('are supported on this server')
         expect(message.middle).toEqual('Test')
+    })
+
+    it('Action message parsing', () => {
+        let msg = `:Test!~Test@test.fr PRIVMSG #Test :\x01ACTION is a test\x01\r\n`
+        let message = new Message(msg)
+        expect(message.prefix.nick).toEqual('Test')
+        expect(message.prefix.user).toEqual('~Test')
+        expect(message.prefix.host).toEqual('test.fr')
+        expect(message.middle).toEqual('#Test')
+        expect(message.trailing).toEqual('\x01ACTION is a test\x01')
+
+        let parser = new Parser()
+        let cmd = parser.parse(message)
+        expect(cmd).toEqual('pubaction')
     })
 })
